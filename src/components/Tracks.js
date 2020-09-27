dbbff7b900acaea85318d651f943b310f274e9ea
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-09-27 05:09:10 -0700

    Commiting changes

--- src/components/Tracks.js
+++ src/components/Tracks.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
 
 class Tracks extends Component {
+  playAudio = (previewUrl) => {
+    const audio = new Audio(previewUrl);
+
+    audio.play();
+  };
+
   render() {
-    const { traccks } = this.props;
+    const { tracks } = this.props;
 
     return (
       <div>
-        {traccks.map((track) => {
-          const { name, album } = tracck;
+        {tracks.map((track) => {
+          const { id, name, album, preview_url } = track;
 
           return (
-            <div key={id}>
+            <div key={id} onClick={this.playAudio(preview_url)}>
               <img src={album.images[0].url} alt='track-image' />
               <p>{name}</p>
             </div>
